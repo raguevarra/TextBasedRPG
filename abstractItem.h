a68ff0ee45a71f644445b482b373486f7fa73599
raguevar <romanguevarra2003@gmail.com>
2023-09-28 10:10:12 -0400

    created abstractItem header

---
+++ abstractItem.h
@@ -0,0 +1,27 @@
+#ifndef ABSTRACTITEM_H
+#define ABSTRACTITEM_H
+
+#include <string>
+
+using namespace std;
+
+class abstractItem {
+
+    // The name of the item (ex. Sealing Cube)
+    string name;
+
+    // The target of the item (ex. player, enemy)
+    string target;
+
+    // The stat that the item affects (ex. Agility)
+    string affect;
+
+    // The number of uses one of this item has
+    int numUses;
+
+    // The amount by which 'affect' is applied
+    int amount;
+    
+};
+
+#endif
