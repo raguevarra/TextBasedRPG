5bacada9c3a2945a78833b1a17b86c1973bbf06b
raguevar <romanguevarra2003@gmail.com>
2023-09-28 10:18:25 -0400

    wrote ctor/dtor

---
+++ abstractItem.cc
@@ -0,0 +1,8 @@
+#include <abstractItem.h>
+#include <string>
+
+// CTOR for an item
+abstractItem::abstractItem(string name, string target, string affect, int numUses, int amount) : name{name}, target{target}, affect{affect}, numUses{numUses}, amount{amount} {}
+
+// DTOR for an item
+abstractItem::~abstractItem() {}
