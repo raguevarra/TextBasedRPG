5c25735351ba017bfc972f38e9b899db7f4ae875
raguevar <romanguevarra2003@gmail.com>
2023-09-28 09:44:21 -0400

    finished abstract item header

---
+++ abstractItem.h
@@ -0,0 +1,22 @@
+#include <string>
+
+using namespace std;
+
+class item {
+
+    // Item name, used for finding in inventory (ex. Sealing Cube)
+    string name;
+
+    // Who to use the item on (enemy, player)
+    string target;
+
+    // Which stat the item impacts
+    string affect;
+
+    // The number of item uses (1, 2, ... , n, ...)
+    int numUses;
+
+    // The amount by which 'affect' applies to the stat
+    int amount;
+    
+};
